4a3077caa94fcc1d154c20f462618b082d706151
niluroy9 <niluroy9@gmail.com>
2020-05-26 03:39:39 +0000

    Lab 5

--- main.js
+++ main.js
@@ -83,69 +83,21 @@ class ServiceNowAdapter extends EventEmitter {
     this.healthcheck();
   }
 
-/**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-    this.emitOffline();
-      log.info('Service now adapter is offline {this.id}');
-      
-      callbackError = error;
-
-
-      /*this.emit('OFFLINE', { id: this.id });
-      log.error(error + ', this.id: ' + this.id);
-      callback(error);*/
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-      this.emitOnline();
-      
-      callbackData = result;
-      log.info('Service now adapter is online');
-
-      /*
-      this.emit('ONLINE', { id: this.id });
-      log.info('ServiceNow: Instance is available.');
-      callback(result);*/
-   }
- });
-}
+  /**
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -200,10 +152,12 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
+     ServiceNowConnector.get(callback);
+     /*
      let getCallOptions = { ...this.props };
     getCallOptions.method = 'GET';
     getCallOptions.query = 'sysparm_limit=1';
-    this.ServiceNowConnector.sendRequest(getCallOptions, (results, error) => callback(results, error));
+    this.ServiceNowConnector.sendRequest(getCallOptions, (results, error) => callback(results, error));*/
   }
 
   /**
@@ -222,9 +176,11 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
+     ServiceNowConnector.post(callback);
+     /*
      let getCallOptions = { ...this.props };
   getCallOptions.method = 'POST';
-  this.ServiceNowConnector.sendRequest(getCallOptions, (results, error) => callback(results, error));
+  this.ServiceNowConnector.sendRequest(getCallOptions, (results, error) => callback(results, error));*/
   }
 }
 
